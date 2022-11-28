bea75a66283b6ec33ad6aae62cc512e7e1708abf
Danail <DanailMinchev@users.noreply.github.com>
2022-11-28 12:26:21 +0000

    feat: add simpleTable example - part 2

--- src/components/simpleTable.tsx
+++ src/components/simpleTable.tsx
@@ -6,23 +6,24 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import {Button} from "@mui/material";
 
 function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
+    charger: string,
+    postcode: string,
+    available: string,
+    avgUptime: string,
+    utilisation: number,
+    kwh: number,
+    warrantyExpires: string,
+    lastMaintenance: string
 ) {
-    return { name, calories, fat, carbs, protein };
+    return { charger, postcode, available, avgUptime, utilisation, kwh, warrantyExpires, lastMaintenance };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('0001', 'E3HFG', 'Yes', '60%', 45, 18.7, '24/09/2023', '08/10/2022'),
+    createData('0002', 'E141PQ', 'No', '20%', 8, 5.8, '03/01/2023', '07/08/2022'),
 ];
 
 export default function BasicTable() {
@@ -31,26 +32,46 @@ export default function BasicTable() {
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Charger</TableCell>
+                        <TableCell align="left">Postcode</TableCell>
+                        <TableCell align="left">Available</TableCell>
+                        <TableCell align="left">Avg. uptime</TableCell>
+                        <TableCell align="left">Utilisation</TableCell>
+                        <TableCell align="left">kWh</TableCell>
+                        <TableCell align="left">Warranty expires</TableCell>
+                        <TableCell align="left">Last Maintenance</TableCell>
+                        <TableCell align="center">Action</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
                         <TableRow
-                            key={row.name}
+                            key={row.charger}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row">
-                                {row.name}
+                                {row.charger}
                             </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="left">{row.postcode}</TableCell>
+                            <TableCell align="left">{row.available}</TableCell>
+                            <TableCell align="left">{row.avgUptime}</TableCell>
+                            <TableCell align="left">{row.utilisation}</TableCell>
+                            <TableCell align="left">{row.kwh}</TableCell>
+                            <TableCell align="left">{row.warrantyExpires}</TableCell>
+                            <TableCell align="left">{row.lastMaintenance}</TableCell>
+                            <TableCell align="center">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    size="small"
+                                    style={{ marginLeft: 16 }}
+                                    onClick={() => {
+                                        return
+                                    }}
+                                >
+                                    Action
+                                </Button>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
