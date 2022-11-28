6d99cbdd6927e4e8379678965602f186d6ff4575
Danail <DanailMinchev@users.noreply.github.com>
2022-11-28 15:20:07 +0000

    feat(simpleTable): use state + UI improvements

--- src/components/simpleTable.tsx
+++ src/components/simpleTable.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,30 +6,31 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {Button} from "@mui/material";
+import {Button, Chip} from "@mui/material";
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 function createData(
     charger: string,
     postcode: string,
-    available: string,
-    avgUptime: string,
+    available: boolean,
+    avgUptime: number,
     utilisation: number,
     kwh: number,
     warrantyExpires: string,
     lastMaintenance: string
 ) {
-    return { charger, postcode, available, avgUptime, utilisation, kwh, warrantyExpires, lastMaintenance };
+    return {charger, postcode, available, avgUptime, utilisation, kwh, warrantyExpires, lastMaintenance};
 }
 
-const rows = [
-    createData('0001', 'E3HFG', 'Yes', '60%', 45, 18.7, '24/09/2023', '08/10/2022'),
-    createData('0002', 'E141PQ', 'No', '20%', 8, 5.8, '03/01/2023', '07/08/2022'),
-];
-
 export default function BasicTable() {
+    const [tableRows, setTableRows] = useState([
+        createData('0001', 'E3HFG', true, 60, 45, 18.7, '24/09/2023', '08/10/2022'),
+        createData('0002', 'E141PQ', false, 20, 8, 5.8, '03/01/2023', '07/08/2022'),
+    ]);
+
     return (
         <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <Table sx={{minWidth: 650}} aria-label="simple table">
                 <TableHead>
                     <TableRow>
                         <TableCell>Charger</TableCell>
@@ -44,17 +45,28 @@ export default function BasicTable() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
+                    {tableRows.map((row) => (
                         <TableRow
                             key={row.charger}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
                         >
                             <TableCell component="th" scope="row">
                                 {row.charger}
                             </TableCell>
                             <TableCell align="left">{row.postcode}</TableCell>
-                            <TableCell align="left">{row.available}</TableCell>
-                            <TableCell align="left">{row.avgUptime}</TableCell>
+                            <TableCell align="left">{row.available ? <Chip label="Yes" style={{
+                                color: 'white',
+                                backgroundColor: 'green'
+                            }}/> : <Chip label="No" style={{
+                                color: 'white',
+                                backgroundColor: 'red'
+                            }}/>}</TableCell>
+                            <TableCell align="left">{row.avgUptime < 50 ? (<span style={{
+                                color: 'red'
+                            }}>{row.avgUptime}% <PriorityHighIcon style={{
+                                color: 'red',
+                                verticalAlign: 'middle'
+                            }}/></span>) : (<>{row.avgUptime}%</>)}</TableCell>
                             <TableCell align="left">{row.utilisation}</TableCell>
                             <TableCell align="left">{row.kwh}</TableCell>
                             <TableCell align="left">{row.warrantyExpires}</TableCell>
@@ -64,9 +76,18 @@ export default function BasicTable() {
                                     variant="contained"
                                     color="primary"
                                     size="small"
-                                    style={{ marginLeft: 16 }}
+                                    style={{marginLeft: 16}}
                                     onClick={() => {
-                                        return
+                                        const newTableRows = tableRows.map(currentRow => {
+                                            if (currentRow.charger !== row.charger) {
+                                                // No change
+                                                return currentRow
+                                            } else {
+                                                return createData(currentRow.charger, currentRow.postcode, !currentRow.available, currentRow.avgUptime, currentRow.utilisation, currentRow.kwh, currentRow.warrantyExpires, currentRow.lastMaintenance)
+                                            }
+                                        })
+                                        // Re-render with the new array
+                                        setTableRows(newTableRows)
                                     }}
                                 >
                                     Action
